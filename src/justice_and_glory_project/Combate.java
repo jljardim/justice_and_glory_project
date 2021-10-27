417f291f74088926c999e4b372b056a341a2313c
Jeferson Jardim <jeferson_luiz@hotmail.com>
2021-10-26 22:14:54 -0300

    inicio_do_projeto

--- src/justice_and_glory_project/Combate.java
+++ src/justice_and_glory_project/Combate.java
@@ -6,8 +6,13 @@ public class Combate {
 	public static void main(String[] args) {
 		System.out.println("Incio");
 		System.out.println("Digite [1] para iniciar ou [2] para sair do jogo");
+		int opcao;
 		Scanner entrada = new Scanner(System.in);
-		String opcaoInicio = entrada.nextLine();
-		System.out.println(opcaoInicio);
+		opcao = entrada.nextInt();
+		if (opcao = 1) {
+		System.out.println("Vamos ao Jogo");
+	}else {
+		System.out.println("Te vejo na Proxima");
 	}
+}
 }
