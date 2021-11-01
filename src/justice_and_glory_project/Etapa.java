0f7f807c968d20ca5ab97af154c16c14093efa6c
Jeferson Jardim <jeferson_luiz@hotmail.com>
2021-11-01 19:01:04 -0300

    classe_etapa

---
+++ src/justice_and_glory_project/Etapa.java
@@ -0,0 +1,77 @@
+package justice_and_glory_project;
+
+public class Etapa {
+	private String textoEtapa;
+    private String opcaoEtapa;
+    private String opcaoEtapa1;
+    private String opcaoEtapa2;
+    private String opcaoEtapa3;
+    
+    public Etapa(String textoEtapa, String opcaoEtapa1, String opcaoEtapa2, String opcaoEtapa3) {
+		this.textoEtapa = textoEtapa;
+		this.opcaoEtapa1 = opcaoEtapa1;
+		this.opcaoEtapa2 = opcaoEtapa2;
+		this.opcaoEtapa3 = opcaoEtapa3;
+	}
+    
+    public Etapa(String textoEtapa) {
+       this.textoEtapa = textoEtapa;
+    }
+    
+    public Etapa() {
+    	
+    }
+    
+    public void obtemNome() {
+     Informacao info = new Informacao();
+    }
+    
+    
+    public String getOpcaoEtapa1() {
+		return opcaoEtapa1;
+	}
+
+	public void setOpcaoEtapa1(String opcaoEtapa1) {
+		this.opcaoEtapa1 = opcaoEtapa1;
+	}
+
+	public String getOpcaoEtapa2() {
+		return opcaoEtapa2;
+	}
+
+	public void setOpcaoEtapa2(String opcaoEtapa2) {
+		this.opcaoEtapa2 = opcaoEtapa2;
+	}
+
+	public String getOpcaoEtapa3() {
+		return opcaoEtapa3;
+	}
+
+	public void setOpcaoEtapa3(String opcaoEtapa3) {
+		this.opcaoEtapa3 = opcaoEtapa3;
+	}
+
+	public boolean verificaEntrada(String entrada) {
+    	if (entrada.equalsIgnoreCase(opcaoEtapa)) {
+    		return true;
+    	}
+    	return false;
+    }
+
+	public String getTextoEtapa() {
+		return textoEtapa;
+	}
+
+	public void setTextoEtapa(String textoEtapa) {
+		this.textoEtapa = textoEtapa;
+	}
+
+	public String getOpcaoEtapa() {
+		return opcaoEtapa;
+	}
+
+	public void setOpcaoEtapa(String opcaoEtapa) {
+		this.opcaoEtapa = opcaoEtapa;
+	}
+	
+}
--- src/justice_and_glory_project/Principal.java
+++ src/justice_and_glory_project/Principal.java
@@ -1,56 +1,99 @@
 package justice_and_glory_project;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Principal {
+	
+	public List<Etapa> etapas;
+	
 	public static void main(String[] args) {
+	
+		Etapa etapa1 = new Etapa("Bem vindo(a) á BATALHA FINAL!\n");
 		
-		System.out.println("OLÁ JOGADOR PRONTO PARA SUA MISSÃO?");
-		System.out.println("\n");
-		System.out.println("Digite [1] para iniciar ou [2] para sair do jogo");
+		Etapa etapa2 = new Etapa("Escolha um nivel de dificuldade. FACIL, NORMAL, DIFICIL.", "FACIL",
+				"NORMAL","DIFICIL");
 		
-		for(int i = 0;i <= 2; i++) {
-			
-		int opcao;
+		Etapa etapa3 = new Etapa("Informe seu nome e sexo. FACIL, NORMAL, DIFICIL.", "JEFERSON",
+				"MASCULINO","FEMININO");
 		
-		Scanner entrada = new Scanner(System.in);
-		
-		opcao = entrada.nextInt();
-		
-		if (opcao == 1) {
-			
-		System.out.println("Seja bem vindo(a) á BATALHA FINAL");
-		
-		System.out.println("Escolha a dificuldade que você quer jogar");
-		
-		System.out.println("Digite [1] para facil, [2] para normal e [3] para dificil");
-		
-		int dificuldade;
-		
-		dificuldade = entrada.nextInt();
-		
-		Dificuldade nivel = new Dificuldade();
-		
-		if(dificuldade  == 1) {
-		System.out.println("OK você quer jogar no nivel: " + nivel.getFacil().toUpperCase());
-		
-		}else if(dificuldade == 2) {
-		System.out.println("OK você quer jogar no nivel: " + nivel.getNormal().toUpperCase());
-		
-		}else if (dificuldade == 3) {
-		System.out.println("OK você quer jogar no nivel: " + nivel.getDificil().toUpperCase());
-		
-		}else {
-			System.out.println("Opção invalida começe o jogo outra vez");
-		}
-			
-		
-	}else if(opcao == 2){
-		System.out.println("Te vejo na Proxima");
-		break;
-	}else {
-		System.out.println("Opção invalida Digite [1] para iniciar ou [2] para sair do jogo ");
+		Etapa etapa4 = new Etapa("Escolha uma classe de combate. GUERREIRO, VIKING, ESTUDANTE.", "GUERREIRO",
+				"VIKING","ESTUDANTE");
+        
+        List<Etapa> etapas = new ArrayList<Etapa>();
+        etapas.add(etapa1);
+        etapas.add(etapa2);
+        etapas.add(etapa3);
+        etapas.add(etapa4);
+        
+       //System.out.println("Bem vindo(a) á BATALHA FINAL!\n");
+        
+        int etapaAtual = 1;
+        System.out.println(etapas.get(0).getTextoEtapa());
+        System.out.println(etapas.get(1).getTextoEtapa());
+        Scanner scannerDif = new Scanner(System.in);
+        String dificuldade = scannerDif.nextLine();
+        if(dificuldade.equalsIgnoreCase(etapas.get(etapaAtual).getOpcaoEtapa1())||
+           dificuldade.equalsIgnoreCase(etapas.get(etapaAtual).getOpcaoEtapa2()) ||
+           dificuldade.equalsIgnoreCase(etapas.get(etapaAtual).getOpcaoEtapa3())) {
+        	
+        	etapaAtual = 2;
+        	//System.out.println(etapaAtual);
+        	System.out.println(etapas.get(etapaAtual).getTextoEtapa());
+        	System.out.println("\n");
+        	System.out.println("------------INFORMAÇÕES DO JOGADOR-------------");
+        	System.out.println("Nome: " + "Jeferson" + "\n" +
+        	"Sexo: " + "Masculino" + "\n" +
+        	"Dificuldade: " + dificuldade + "\n" +
+        	"Classe de combate: " + "Guerreiro");
+        	System.out.println("------------------------------------------------");
+        }
+//		System.out.println("OLÁ JOGADOR PRONTO PARA SUA MISSÃO? \n");
+//		System.out.println("Digite [1] para iniciar ou [2] para sair do jogo \n");
+//		
+//		int opcao;
+//				
+//	    Scanner entrada = new Scanner(System.in);
+//				
+//		opcao = entrada.nextInt();
+//			
+//		if (opcao == 1) {
+//			
+//		System.out.println("Seja bem vindo(a) á BATALHA FINAL \n");
+//		}else if(opcao == 2){
+//		System.out.println("Te vejo na Proxima");
+//		System.exit(0);
+//		}else {
+//		System.out.println("Opção invalida começe novamente.");
+//		System.exit(0);
+//		}
+//		
+//		System.out.println("Escolha a dificuldade que você quer jogar \n");
+//		
+//		System.out.println("Digite [1] para facil, [2] para normal e [3] para dificil");
+//		
+//		int dificuldade;
+//		
+//		Scanner entrada2 = new Scanner(System.in);
+//		
+//		dificuldade = entrada2.nextInt();
+//		
+//		Dificuldade nivel = new Dificuldade();
+//		
+//		if(dificuldade  == 1) {
+//		System.out.println("OK você quer jogar no nivel: " + nivel.getFacil().toUpperCase());
+//		
+//		}else if(dificuldade == 2) {
+//		System.out.println("OK você quer jogar no nivel: " + nivel.getNormal().toUpperCase());
+//		
+//		}else if (dificuldade == 3) {
+//		System.out.println("OK você quer jogar no nivel: " + nivel.getDificil().toUpperCase());
+//		
+//		}else {
+//			System.out.println("Opção invalida começe o jogo outra vez");
+//		}
+//			
+//		}
 	}
-  }
-}
 }
